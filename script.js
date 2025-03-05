0ad3a7ea25a2fa46ac0d47f8c3c07b071b2d3155
Magooch <crvallecito@gmail.com>
2025-03-04 20:04:34 -0700

    Create event listener to switch time

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 let am = document.getElementById('am')
 let pm = document.getElementById('pm')
+let date = document.getElementById('date')
+let mTime = document.getElementById('mTimeBtn')
+let display = document.getElementById('clock')
 
+mTimeBool = false
+
+mTimeBtn.addEventListener('click', function() {
+    if (mTimeBool === false) {
+        display.textContent = "22:55"
+        mTimeBool = true
+    } else {
+        mTimeBool = false
+        display.textContent = "10:55"
+    }
+})
